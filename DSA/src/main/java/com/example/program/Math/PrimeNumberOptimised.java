867c47d1c2dc35c02aa9420f48614fc6e47afb8b
Rahul Kamble <rahul.kamble1093@gmail.com>
2025-07-28 08:53:22 +0530

    Prime and Efficient Prime Program

--- DSA/src/main/java/com/example/program/Math/PrimeNumberOptimised.java
+++ DSA/src/main/java/com/example/program/Math/PrimeNumberOptimised.java
@@ -1,17 +1,16 @@
 package com.example.program.Math;
 
-public class PrimeNumberProgram {
+public class PrimeNumberOptimised {
     public static void main(String[] args) {
-        System.out.println(checkPrime(4));
+        System.out.println(primeNumberOptimiesd(6));
     }
 
-    public static boolean checkPrime(int n){
+    public static boolean primeNumberOptimiesd(int n){
         // return false if n=1
         if (n==1) return false;
-        for(int i=2; i<n; i++)
+        for(int i=2; i*i<n; i++)
             if(n % i ==0)
                 return false;
         return true;
     }
-
 }
---
+++ DSA/src/main/java/com/example/program/Math/PrimeNumberOptimisedMore.java
@@ -0,0 +1,18 @@
+package com.example.program.Math;
+
+public class PrimeNumberOptimisedMore {
+    public static void main(String[] args) {
+        System.out.println(primeNumberOptimiesdMore(1031));
+    }
+
+    public static boolean primeNumberOptimiesdMore(int n){
+        // return false if n=1
+        if (n==1) return false;
+        if(n==2 || n==3) return true;
+        if(n%2 ==0 || n%3==0) return false;
+        for(int i=5; i*i<n; i=i+6)
+            if(n % i ==0 || n % (i+2)==0)
+                return false;
+        return true;
+    }
+}
