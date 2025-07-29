a1ca330674cc4069bfa746850b3c025b880fe8a5
Rahul Kamble <rahul.kamble1093@gmail.com>
2025-07-29 07:53:18 +0530

    Prime Factor More Efficient Program

--- DSA/src/main/java/com/example/program/Math/PrimeFactors.java
+++ DSA/src/main/java/com/example/program/Math/PrimeFactors.java
@@ -5,6 +5,8 @@ public class PrimeFactors {
         calculatePrimeFactors(450);
         System.out.println("\n");
         calculatePrimeFactorEfficient(450);
+        System.out.println("\n");
+        calculatePrimeFactorMoreEfficient(450);
     }
 
     public static void calculatePrimeFactors(int n){
@@ -30,4 +32,32 @@ public class PrimeFactors {
         if(n>1)
             System.out.println(n);
     }
+
+    public static void calculatePrimeFactorMoreEfficient(int n){
+        if(n<=1)
+            return;
+        while(n%2==0){
+            System.out.println(2);
+            n=n/2;
+        }
+        while(n%3==0){
+            System.out.println(3);
+            n=n/3;
+        }
+        for (int i=5; i*i<=n; i=i+6){
+            while(n%i==0){
+                System.out.println(i);
+                n=n/i;
+            }
+            while(n%(i+2)==0){
+                System.out.println(i+2);
+                n=n/(i+2);
+            }
+        }
+        if(n>3)
+            System.out.println(n);
+
+    }
+
+
 }
